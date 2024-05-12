bec37d718dc0f4848b00335be735e0a6a0dfb029
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2024-05-12 15:03:51 +0530

    hiring problem changed

--- AA/hiring_random.py
+++ AA/hiring_random.py
@@ -1,36 +1,34 @@
 import random
- 
-print("***********HIRING PROBLEM (RANDOMIZE)***********")
-print("----------------------------")
-print("Cand   IC        HC       TC")
-print("----------------------------")
- 
-candidates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-interviewed_candidates = []
+
+candidates = [1, 6, 18, 32, 14, 45, 31, 28, 64, 52, 81, 22, 35, 77, 47]
 hired_candidates = []
-total_cost = 0
+fired_candidates = [] 
 interview_cost = 10
- 
-for i in range(len(candidates)):
+total_cost = 0
+
+print('-------------------------------')
+print('Cand      IC      HC      TC')
+print('-------------------------------')
+
+max = -1
+for _ in range(len(candidates)):
+    hc = 0
     selected_candidate = random.choice(candidates)
-    interviewed_candidates.append(selected_candidate)
+    if selected_candidate > max:
+        if max!=-1:
+            fired_candidates.append(max)
+        hired_candidates.append(selected_candidate)
+        max = selected_candidate
+        hc += 1
     candidates.remove(selected_candidate)
- 
- 
-max=-1
-for i in range(len(interviewed_candidates)):
-    hc=0
-    if interviewed_candidates[i] > max:
-        max=interviewed_candidates[i]
-        hired_candidates.append(interviewed_candidates[i])
-        hc+=1
-    print(f"{interviewed_candidates[i]}      {interview_cost}        {hc*interview_cost}       {interview_cost+(hc*interview_cost)}")
-    total_cost+=interview_cost+(hc*interview_cost)
- 
-print()
-print("Interviewed candidates:", interviewed_candidates)
-print("Hired candidates:", hired_candidates)
-print("Number of candidates hired:", len(hired_candidates))
-print("Hiring Cost: ", interview_cost*len(hired_candidates))
-print("Firing Cost: ", interview_cost*(len(hired_candidates)-1))
-print("Total Cost: ",total_cost)
+    total_cost += interview_cost + (hc*interview_cost)
+    print(f'{selected_candidate}         {interview_cost}       {hc*interview_cost}       {interview_cost + (hc*interview_cost)}')
+
+print(f'List of Hired candidates till now: {hired_candidates}')
+print(f'Total Hiring Cost: {len(hired_candidates)*interview_cost}')
+print(f'List of Fired Candidates: {fired_candidates}')
+print(f'Total Firing Cost: {len(fired_candidates)*interview_cost}')
+print(f'Current Hired Assistant: {max}')
+print(f'Total Cost: {total_cost}')
+
+
--- AA/tt.py
+++ AA/tt.py
@@ -1,4 +1,34 @@
 import random
 
-for i in range(30):
-    print(random.randint(1,10))
+candidates = [1, 6, 18, 32, 14, 45, 31, 28, 64, 52, 81, 22, 35, 77, 47]
+hired_candidates = []
+fired_candidates = [] 
+interview_cost = 10
+total_cost = 0
+
+print('-------------------------------')
+print('Cand      IC      HC      TC')
+print('-------------------------------')
+
+max = -1
+for _ in range(len(candidates)):
+    hc = 0
+    selected_candidate = random.choice(candidates)
+    if selected_candidate > max:
+        if max!=-1:
+            fired_candidates.append(max)
+        hired_candidates.append(selected_candidate)
+        max = selected_candidate
+        hc += 1
+    candidates.remove(selected_candidate)
+    total_cost += interview_cost + (hc*interview_cost)
+    print(f'{selected_candidate}         {interview_cost}       {hc*interview_cost}       {interview_cost + (hc*interview_cost)}')
+
+print(f'List of Hired candidates till now: {hired_candidates}')
+print(f'Total Hiring Cost: {len(hired_candidates)*interview_cost}')
+print(f'List of Fired Candidates: {fired_candidates}')
+print(f'Total Firing Cost: {len(fired_candidates)*interview_cost}')
+print(f'Current Hired Assistant: {max}')
+print(f'Total Cost: {total_cost}')
+
+
